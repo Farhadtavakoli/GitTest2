3a7580ea8fd32fa8f54a1d5d1d8857216c761042
Farhad Tavakoli <farhad.tavakoli81@yahoo.com>
2020-10-22 14:44:29 +0200

    Add project files.

---
+++ GitTest2/Program.cs
@@ -0,0 +1,18 @@
+﻿using System;
+
+namespace GitTest2
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            for(int i=1;i<=10;i++)
+            {
+                Console.WriteLine("{0}^2 = {1} ", i, i * i);
+                 
+            }
+            Console.ReadKey();
+
+        }
+    }
+}

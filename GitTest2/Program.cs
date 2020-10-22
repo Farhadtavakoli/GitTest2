6f9568f33b4ef92c7b837063c3654e3d43b28974
Farhad Tavakoli <farhad.tavakoli81@yahoo.com>
2020-10-22 14:57:38 +0200

    ^3

--- GitTest2/Program.cs
+++ GitTest2/Program.cs
@@ -8,7 +8,7 @@ namespace GitTest2
         {
             for(int i=1;i<=10;i++)
             {
-                Console.WriteLine("{0}^2 = {1} ", i, i * i); // It was just a test
+                Console.WriteLine("{0}^3 = {1} ", i, i*i*i); // It was just a test
                   
             }
             Console.ReadKey();
